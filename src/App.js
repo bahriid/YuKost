d7345cfb7f24906bb3b7c503ed43b48ebd7ae53d
aribahri15 <aribahri15@gmail.com>
2020-05-20 22:11:52 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">Hello World
+    </div>
+  );
+}
+
+export default App;
